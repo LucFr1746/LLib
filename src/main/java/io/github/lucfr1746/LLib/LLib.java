2b43d401a77fb6a68d4dff27540a0cbba25a5cd5
Thế Lực <LucFr.1746@gmail.com>
2024-10-20 18:20:45 +0700

    Base Update

---
+++ src/main/java/io/github/lucfr1746/LLib/LLib.java
@@ -0,0 +1,17 @@
+package io.github.lucfr1746.LLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
